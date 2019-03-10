4c907c23222b47e04886abaffb582502d2a81d71
Alec Palmans <alec.palmans@gmail.com>
2019-03-10 12:52:21 +0100

    Hele oefening van array prijzen

--- Foreach_Prijzen/Program.cs
+++ Foreach_Prijzen/Program.cs
@@ -10,6 +10,23 @@ namespace Foreach_Prijzen
     {
         static void Main(string[] args)
         {
+            double[] prijzen = new double[20];
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Geef 20 prijzen in. {prijzen[i]}/20");
+                double prijs = Convert.ToDouble(Console.ReadLine());
+                prijzen[i] = prijs;
+            }
+            foreach (double prize in prijzen)
+            {
+                if (prize >= 5.00)
+                {
+                    Console.WriteLine(prize);
+                }
+            }
+            double average = prijzen.Average();
+            Console.WriteLine("Het gemiddelde van alle prijzen is: " + average);
+            Console.ReadLine();
         }
     }
 }
